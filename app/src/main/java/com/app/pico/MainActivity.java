b6e4beb655c3bf2cd5c3b902be89015c37bccba8
Bren101 <b101@umbc.edu>
2017-04-30 02:37:05 -0400

    Main activity settings button goes to settings page. New settings page. UI basically done, updates fields in the java class when user changes values

--- app/src/main/java/com/app/pico/MainActivity.java
+++ app/src/main/java/com/app/pico/MainActivity.java
@@ -1,10 +1,13 @@
 package com.app.pico;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
     private StyledTextHeader logo;
     private StyledButton btnAlarms;
     private StyledButton btnLocations;
@@ -21,7 +24,43 @@ public class MainActivity extends AppCompatActivity {
         btnAlarms = (StyledButton) findViewById(R.id.btnAlarm);
         btnLocations = (StyledButton) findViewById(R.id.btnLocations);
         btnSettings = (StyledButton) findViewById(R.id.btnSettings);
+        btnSettings.setOnClickListener(this);
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        btnAlarms.setOnClickListener(this);
+        btnLocations.setOnClickListener(this);
+        btnSettings.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        btnAlarms.setOnClickListener(null);
+        btnLocations.setOnClickListener(null);
+        btnSettings.setOnClickListener(null);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()){
+
+            case R.id.btnAlarm:
+                break;
+
+            case R.id.btnLocations:
+                break;
+
+            case R.id.btnSettings:
+                Intent settingsIntent = new Intent(this, SettingsActivity.class);
+                startActivity(settingsIntent);
+                break;
+
+        }
+
+    }
 }
---
+++ app/src/main/java/com/app/pico/SettingsActivity.java
@@ -0,0 +1,169 @@
+package com.app.pico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.NumberPicker;
+import android.widget.Spinner;
+import android.widget.ToggleButton;
+
+public class SettingsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    boolean suRep, moRep, tuRep, weRep, thRep, frRep, saRep;
+    Integer snoozeTime;
+    String sound;
+
+    ToggleButton suTog, moTog, tuTog, weTog, thTog, frTog, saTog;
+    NumberPicker snoozePicker;
+    Spinner soundSpinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        suRep = false;
+        moRep = false;
+        tuRep = false;
+        weRep = false;
+        thRep = false;
+        frRep = false;
+        saRep = false;
+        snoozeTime = 5;
+        sound = "";
+
+        suTog = (ToggleButton) findViewById(R.id.sundayToggle);
+        moTog = (ToggleButton) findViewById(R.id.mondayToggle);
+        tuTog = (ToggleButton) findViewById(R.id.tuesdayToggle);
+        weTog = (ToggleButton) findViewById(R.id.wednesdayToggle);
+        thTog = (ToggleButton) findViewById(R.id.thursdayToggle);
+        frTog = (ToggleButton) findViewById(R.id.fridayToggle);
+        saTog = (ToggleButton) findViewById(R.id.saturdayToggle);
+
+        snoozePicker = (NumberPicker) findViewById(R.id.numberPicker);
+        snoozePicker.setMinValue(1);
+        snoozePicker.setMaxValue(60);
+        snoozePicker.setValue(5);
+        snoozePicker.setWrapSelectorWheel(true);
+
+        soundSpinner = (Spinner) findViewById(R.id.soundSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.sounds_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        soundSpinner.setAdapter(adapter);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        suTog.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    suRep = true;
+                } else {
+                    suRep = false;
+                }
+            }
+        });
+        moTog.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    moRep = true;
+                } else {
+                    moRep = false;
+                }
+            }
+        });
+        tuTog.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    tuRep = true;
+                } else {
+                    tuRep = false;
+                }
+            }
+        });
+        weTog.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    weRep = true;
+                } else {
+                    weRep = false;
+                }
+            }
+        });
+        thTog.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    thRep = true;
+                } else {
+                    thRep = false;
+                }
+            }
+        });
+        frTog.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    frRep = true;
+                } else {
+                    frRep = false;
+                }
+            }
+        });
+        saTog.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    saRep = true;
+                } else {
+                    saRep = false;
+                }
+            }
+        });
+
+        snoozePicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                snoozeTime = newVal;
+            }
+        });
+
+        soundSpinner.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        suTog.setOnCheckedChangeListener(null);
+        moTog.setOnCheckedChangeListener(null);
+        tuTog.setOnCheckedChangeListener(null);
+        weTog.setOnCheckedChangeListener(null);
+        thTog.setOnCheckedChangeListener(null);
+        frTog.setOnCheckedChangeListener(null);
+        saTog.setOnCheckedChangeListener(null);
+
+        snoozePicker.setOnValueChangedListener(null);
+
+        soundSpinner.setOnItemSelectedListener(null);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        sound = parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        sound = "";
+    }
+}
+
